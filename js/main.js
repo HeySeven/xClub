be20ebe668d08ee9dc225b61621df705e398400c
shang <shangxp@wikiflyer.cn>
2016-09-27 03:22:09 +0800

    9.26

--- js/main.js
+++ js/main.js
@@ -1,28 +1,196 @@
-$(function() {
-    if( !('placeholder' in document.createElement('input')) ){
-        $('input[placeholder],textarea[placeholder]').each(function(){
+$(function () {
+    if (!('placeholder' in document.createElement('input'))) {
+        $('input[placeholder],textarea[placeholder]').each(function () {
             var that = $(this),
-                text= that.attr('placeholder');
-            if(that.val()===""){
+                text = that.attr('placeholder');
+            if (that.val() === "") {
                 that.val(text).addClass('placeholder');
             }
-            that.focus(function(){
-                    if(that.val()===text){
+            that.focus(function () {
+                    if (that.val() === text) {
                         that.val("").removeClass('placeholder');
                     }
                 })
-                .blur(function(){
-                    if(that.val()===""){
+                .blur(function () {
+                    if (that.val() === "") {
                         that.val(text).addClass('placeholder');
                     }
                 })
-                .closest('form').submit(function(){
-                if(that.val() === text){
+                .closest('form').submit(function () {
+                if (that.val() === text) {
                     that.val('');
                 }
             });
         });
     }
+
+    //标签弹窗
+    $('[data-toggle="popover-tag"]').webuiPopover({
+        placement: 'auto',
+        delay: {
+            show: null,
+            hide: 100
+        },
+        trigger: 'hover',
+        cache: true
+    });
+
+
+    //用户信息弹窗
+    var userData = {
+        'name': 'Ron Heard',
+        'position': '阿里巴巴 产品经理',
+        'acceptCount': 1208,
+        'fansCount': 122,
+        'isFans': false
+    }
+    var htmlString = '<div class="user-pop">' +
+        '        <div class="user-img">' +
+        '            <img src="images/person.png" alt="">' +
+        '        </div>' +
+        '        <dl>' +
+        '            <dt>' + userData.name + '</dt>' +
+        '            <dd>' + userData.position + '</dd>' +
+        '        </dl>' +
+        '        <ul>' +
+        '            <li>' +
+        '                <span>认可</span>' +
+        '                <strong>' + userData.acceptCount + '</strong>' +
+        '            </li>' +
+        '            <li class="last">' +
+        '                <span>粉丝</span>' +
+        '                <strong>' + userData.fansCount + '</strong>' +
+        '            </li>' +
+        '        </ul>' +
+        '        <div class="gz-btn">' +
+        '            <button data-toggle="fans" class="btn ' + (userData.isFans ? 'outline-btn' : 'btn-success') + ' btn-block" type="button">' + (userData.isFans ? '取消关注' : '关注') + ' </button>' +
+        '        </div>' +
+        '    </div>';
+
+    $('[data-toggle="user-info"]').webuiPopover({
+        placement: 'auto',
+        type: 'html',
+        style: 'inverse',
+        content: htmlString,
+        delay: {
+            show: null,
+            hide: 100
+        },
+        trigger: 'hover',
+        cache: true
+    });
+
+    //关注用户
+    $(document).on('click', '[data-toggle="fans"]', function () {
+        var $this = $(this);
+        if ($this.hasClass('outline-btn')) {
+            $this.removeClass('outline-btn').addClass('btn-success').text('关注');
+        } else {
+            $this.removeClass('btn-success').addClass('outline-btn').text('取消关注');
+        }
+    });
+
+    //toggle-favo
+    $('[data-toggle="toggle-favo"]').click(function () {
+        var $this = $(this);
+        if ($this.hasClass('active')) {
+            $this.removeClass('active').html('<span class="iconfont icon-favo"></span> 收藏');
+        } else {
+            $this.addClass('active').html('<span class="iconfont icon-favo"></span> 取消收藏');
+        }
+    });
+    //toggle-active
+    $('[data-toggle="toggle-active"]').click(function () {
+        var $this = $(this);
+        if ($this.hasClass('active')) {
+            $this.removeClass('active').html('<span class="iconfont icon-focus"></span> 关注该问题');
+        } else {
+            $this.addClass('active').html('<span class="iconfont icon-focus"></span> 已关注');
+        }
+    });
+    //toggle-invite
+    $('[data-toggle="toggle-invite"]').click(function () {
+        var $this = $(this);
+        $this.closest('.list-inline').next('.invite-box').stop(true, true).fadeToggle();
+    });
+    //toggle-comment
+    $('[data-toggle="comment"]').click(function () {
+        var $this = $(this);
+        $this.closest('.pmcaff-panel-footer').next('.comment').stop(true, true).fadeToggle();
+    });
+    //invite
+    $('[data-toggle="invite"]').click(function () {
+        var $this = $(this);
+        if (!$this.hasClass('active')) {
+            $this.addClass('active').text('已邀请');
+        }
+    });
+
+    //
+    $('[data-toggle="control-btn"]').webuiPopover({
+        placement: 'auto-bottom',
+        delay: {
+            show: null,
+            hide: 100
+        },
+        trigger: 'hover',
+        cache: true
+    });
+    //toggle-active
+    $('.btn-add').click(function () {
+        var $this = $(this);
+        var count = parseInt($this.find('span').text());
+        if ($this.hasClass('active')) {
+            $this.removeClass('active').find('span').text(count - 1);
+        } else {
+            $this.addClass('active').find('span').text(count + 1);
+        }
+    });
+
+    $('[data-toggle="confirmation"]').confirmation({
+        title: '确认删除？',
+        popout: true,
+        btnOkClass: 'btn-success',
+        btnOkLabel: '确定',
+        btnCancelLabel: '取消',
+        onConfirm: function () {
+            alert('点击了确定');
+        },
+        onCancel: function () {
+            alert('点击了取消');
+        }
+    });
+
+
+
+    //富文本回复
+    var editor = new Simditor({
+        textarea: $('[data-toggle="textarea-simditor"]')
+        //optional options
+    });
+    var $answerInput = $('.answer-input');
+    var $answerEditWrap = $('.answer-edit-wrap')
+    $answerInput.click(function(){
+        showEdit()
+    });
+    $(".answer-input-wrap").click(function(event){
+        event.stopPropagation();
+    });
+    $(document).click(function(){
+        hideEdit();
+    });
+    function showEdit(){
+        $answerInput.hide();
+        $answerEditWrap.fadeIn('slow');
+    }
+    function hideEdit(){
+        $answerEditWrap.hide();
+        $answerInput.fadeIn('slow');
+    }
+
+    $('#is-anonymous').change(function(){
+        $(this).parent('label').toggleClass("checked");
+    });
 
 
 
